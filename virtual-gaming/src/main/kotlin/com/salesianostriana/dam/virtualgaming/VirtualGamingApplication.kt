7b94f948641c101209cd334e0771d90bdbf1a4c4
JuanGCM <chavezmarengo@hotmail.com>
2021-03-15 19:33:19 +0100

    base construida

---
+++ virtual-gaming/src/main/kotlin/com/salesianostriana/dam/virtualgaming/VirtualGamingApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.virtualgaming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VirtualGamingApplication
+
+fun main(args: Array<String>) {
+	runApplication<VirtualGamingApplication>(*args)
+}
