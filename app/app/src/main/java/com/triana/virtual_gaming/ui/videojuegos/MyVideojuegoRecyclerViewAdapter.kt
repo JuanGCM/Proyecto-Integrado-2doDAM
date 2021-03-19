7193a037bd73ef5e283798b363722a1f7babc566
JuanGCM <chavezmarengo@hotmail.com>
2021-03-19 17:16:23 +0100

    fronted-base-ajustes

--- app/app/src/main/java/com/triana/virtual_gaming/ui/videojuegos/MyVideojuegoRecyclerViewAdapter.kt
+++ app/app/src/main/java/com/triana/virtual_gaming/ui/videojuegos/MyVideojuegoRecyclerViewAdapter.kt
@@ -1,4 +1,12 @@
 package com.triana.virtual_gaming.ui.videojuegos
 
-class MyVideojuegoRecyclerViewAdapter {
+import androidx.recyclerview.widget.RecyclerView
+import com.example.realestate.ui.UnaVivienda
+import com.example.realestate.ui.viviendas.MyViviendaRecyclerViewAdapter
+
+class MyVideojuegoRecyclerViewAdapter(
+    private var values: List<UnaVivienda>
+) : RecyclerView.Adapter<MyVideojuegoRecyclerViewAdapter.ViewHolder>() {
+
+
 }
